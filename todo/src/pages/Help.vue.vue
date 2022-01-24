194afff36b431c81ce93396f6d1b97cbb6662ac7
adnan <adnan.syria.91@gmail.com>
2022-01-24 17:45:44 +0330

    first quasar project

---
+++ todo/src/pages/Help.vue.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="q-pa-lg" >
+    <h5 class="q-mt-none"> Help </h5>
+    <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus asperiores excepturi, repudiandae maxime, corrupti aperiam pariatur eveniet officia odit eos at voluptates iusto accusantium incidunt veniam reiciendis molestiae ipsum possimus? </p>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus asperiores excepturi, repudiandae maxime, corrupti aperiam pariatur eveniet officia odit eos at voluptates iusto accusantium incidunt veniam reiciendis molestiae ipsum possimus? </p>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
---
+++ todo/src/pages/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+      <q-item v-for="task in tasks" :key="task.title" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" val="teal" color="secondary" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label> {{ task.title }} </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: "red",
+      tasks: [
+        { title: "Get banana", done: false },
+        { title: "Eat banana", done: true },
+        { title: "Poo banana", done: false },
+      ],
+    };
+  },
+};
+</script>
